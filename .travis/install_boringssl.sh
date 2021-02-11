c3348a6c9b5b0410c3002ee60668354d0ff9cd82
Chris Papazian <papazian@pinterest.com>
2021-02-11 18:29:15 +0000

    fix wget

--- .travis/install_boringssl.sh
+++ .travis/install_boringssl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 BORINGSSL="chromium-stable"
 BORINGSSL_ZIP="boringssl-${BORINGSSL}.zip"
-travis_retry wget --no-check-certificate -o boringssl-${BORINGSSL}.zip https://github.com/google/boringssl/archive/${BORINGSSL}.zip 
-echo "c8306c37c9f0e0a88ff13a90e2a8f468  ${BORINGSSL_ZIP}" > boringssl_md5.txt
+travis_retry wget --no-check-certificate -O ${BORINGSSL_ZIP} https://github.com/google/boringssl/archive/${BORINGSSL}.zip 
+echo "1acff89fe89096959156b337493dd524  ${BORINGSSL_ZIP}" > boringssl_md5.txt
 md5sum -c boringssl_md5.txt
 unzip ${BORINGSSL_ZIP}
 pushd boringssl-${BORINGSSL}
