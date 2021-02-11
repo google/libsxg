2017ca04ef2ff095ce55d1fda9e0e07d678ec3b3
Chris Papazian <papazian@pinterest.com>
2021-02-11 23:18:00 +0000

    set BORINGSSL_ROOT to help find boringssl

--- .travis/install_boringssl.sh
+++ .travis/install_boringssl.sh
@@ -6,11 +6,11 @@ echo "1acff89fe89096959156b337493dd524  ${BORINGSSL_ZIP}" > boringssl_md5.txt
 md5sum -c boringssl_md5.txt
 unzip ${BORINGSSL_ZIP}
 pushd boringssl-${BORINGSSL}
+export BORINGSSL_ROOT=`pwd`
 mkdir build
 cd build
 cmake -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC .. 
 make -j`nproc`
-export BORINGSSL_ROOT=`pwd`
 popd
 find . -name libcrypto.a
 find . -name libssl.a
